f659c7681a2801f17604a798f3c53940a0bd8889
sontt1 <tstran95>
2022-06-27 15:09:59 +0700

    fix get Remaining time

--- src/main/java/com/redis/utils/ProductUtils.java
+++ src/main/java/com/redis/utils/ProductUtils.java
@@ -1,13 +1,15 @@
 package com.redis.utils;
 
-import java.text.ParseException;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 public class ProductUtils {
     public static long getTimeRemaining() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(calendar.get(Calendar.YEAR) , calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH) , 23 , 59 , 59);
-        return (calendar.getTimeInMillis() - Calendar.getInstance().getTimeInMillis())  / 1000;
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(calendar.get(Calendar.YEAR) , calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH) , 23 , 59 , 59);
+//        return (calendar.getTimeInMillis() - Calendar.getInstance().getTimeInMillis())  / 1000;
+        return ChronoUnit.SECONDS.between( LocalDateTime.now() , LocalDate.now().atTime(LocalTime.MAX));
     }
 }
